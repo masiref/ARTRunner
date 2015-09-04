af116bb37d10356ad6c1c4f2166b7fcec4b89d58
todonte <masire.fofana@gmail.com>
2015-09-04 16:02:54 +0200

    First commit

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,37 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+{
+
+    private $output;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Then I should see :title modal
+     */
+    public function iShouldSeeModal($title)
+    {
+        sleep(1);
+        $this->assertPageContainsText($title);
+    }
+}
